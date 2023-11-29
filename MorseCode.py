c1c57f5c45f51e348beeb47e554c3209bcc4e0d3
Alhossain333 <AK2200048@tkh.edu.eg>
2023-11-29 22:39:09 +0200

    meaningful comments

--- MorseCode.py
+++ MorseCode.py
@@ -22,6 +22,7 @@ def encrypt(plain_message):
     plain_message = plain_message.upper()
     encryptedMessage = []
     for plainChar in plain_message:
+       # to check if that charecter is in our predefined dict
         if plainChar in Morse_code_mapping:
             morseCode = Morse_code_mapping.get(plainChar)
             encryptedMessage.append(morseCode)
@@ -35,10 +36,13 @@ def decrypt(encrypted_message):
     # convert encrypted_message string into array
     morse_codes = encrypted_message.split(' ')
     decrypted_message = []
+    # Iterate through each Morse code in the input message
     for code in morse_codes:
+        # Iterate through the Morse code mapping to find the corresponding character
         for char, morse_code in Morse_code_mapping.items():
             if code == morse_code:
                 decrypted_message.append(char)
+    # Join the decrypted characters to form the final decrypted message
     decryptedmessegeString = ''.join(decrypted_message)
     print(decryptedmessegeString)
     return decryptedmessegeString
